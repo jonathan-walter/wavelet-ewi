d5ac8ad06f0bc0446a89cc72e30c94e3e086fe27
jonathan-walter <jaw3es@virginia.edu>
2020-04-20 19:05:43 -0400

    initial package build

---
+++ Package/wtCNN/build.R
@@ -0,0 +1,12 @@
+#rm(list=ls())
+library(roxygen2)
+library(devtools)
+
+setwd("~/GitHub/wavelet-ewi/Package/")
+
+#create("wtCNN")
+build("wtCNN")   # need a folder named 'wtCNN, in which a file named DESCRIPTION and a sub-folder named 'R'. This will creat a zip file
+document("wtCNN") # this will create a file 'NAMESPACE' and a folder 'man'
+check("wtCNN")
+install("wtCNN")
+library("wtCNN")
